d88490751639f388164870d1e339f8f2e3c6433d
jschenkelberg <jmschenkelberg@gmail.com>
2021-07-22 10:09:25 -0500

    fixing repo issue

--- youTubeClone/comment/models.py
+++ youTubeClone/comment/models.py
@@ -4,5 +4,5 @@ class Comment(models.Model):
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     video_id = models.CharField(max_length=100)
-    previous_comment = models.ForeignKey('comment.Comment', default=0, on_delete=models.PROTECT)
+    previous_comment = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
     new_comment = models.CharField(max_length=100)
