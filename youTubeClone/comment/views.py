5567cb27c1de0d934312906d251e0a7f26ade51b
jschenkelberg <jmschenkelberg@gmail.com>
2021-07-22 11:49:35 -0500

    api requests comfirmed

--- youTubeClone/comment/views.py
+++ youTubeClone/comment/views.py
@@ -1,20 +1,55 @@
-from django.shortcuts import render
 from .models import Comment
 from .serializers import CommentSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
+
 
 class CommentList(APIView):
 
-    def get(self, request):
+    @staticmethod
+    def get(request):
         comment = Comment.objects.all()
         serializer = CommentSerializer(comment, many=True, partial=True)
         return Response(serializer.data)
 
+    @staticmethod
     def post(self, request):
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetail(APIView):
+    @staticmethod
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def patch(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            comment.likes += 1
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            comment.dislikes += 1
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = self.get_object(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
